e94361c4961de78fe2048c87fbc3f193d4c80934
unknown <khangweloramanenga@gmail.com>
2023-06-12 08:48:37 +0200

    re-admission

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
  const baseUrl = 'http://localhost:8080/api/login'
-// const baseUrl = 'http://192.168.27.52/api/login'
+//const baseUrl = 'http://192.168.27.52/api/login'
 @Injectable({
   providedIn: 'root'
 })
