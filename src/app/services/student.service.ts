5b1fa49e6d809dd4b521187f01b9e93bdd9fc209
unknown <khangweloramanenga@gmail.com.com>
2023-06-13 10:32:34 +0200

    Updated

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -8,8 +8,8 @@ import { ReAdmissionComponent } from '../components/re-admission/re-admission.co
   providedIn: 'root'
 })
 export class StudentService {
-  private baseUrl = 'http://localhost:8080/api';
-  // private BaseUrl = 'http://192.168.27.46:8080/api'
+  //private baseUrl = 'http://localhost:8080/api';
+  private BaseUrl = 'http://192.168.27.52:8080/api'
 
   constructor(private http: HttpClient) { }
 
@@ -19,27 +19,27 @@ recLetter(student_no:any): Observable<any>
 {
   
   // return this.http.get<any>(`${this.baseUrl}/` +student_no);
-  return this.http.get<any>(`${this.baseUrl}/info/` +student_no);
+  return this.http.get<any>(`${this.BaseUrl}/info/` +student_no);
 }
 
 epForm(data: any): Observable<any>
 {
-  return this.http.post<any>(`${this.baseUrl}/forms`, data );
+  return this.http.post<any>(`${this.BaseUrl}/forms`, data );
 }
 
 uploadPlacementLetter(data: any): Observable<any> 
 {
-  return this.http.post<any>(`${this.baseUrl}/uploadFolder`, data )
+  return this.http.post<any>(`${this.BaseUrl}/uploadFolder`, data )
 };
 
 ReAdmission(data: any):Observable<any> 
 {
-  return this.http.post<any>(`${this.baseUrl}/admform`, data )
+  return this.http.post<any>(`${this.BaseUrl}/admform`, data )
 };
 
 uploadSystemDocumentation(data: any): Observable<any>
 {
- return this.http.post<any>(`${this.baseUrl}/uploadSysDoc`, data)
+ return this.http.post<any>(`${this.BaseUrl}/uploadSysDoc`, data)
 }
 
 
